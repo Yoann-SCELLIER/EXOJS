82c7596aab02c97a3451677c6df9971b85bc0602
kferrerux <ferrerkevin@hotmail.fr>
2024-01-08 14:24:15 +0100

    random math.floor exo

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
 // YOANN & KEVIN - EXO JavaScript */
 
-// On déclare une variable let
-let number = 10;
+// On déclare une variable let (pour un chiffre déterminé)
+// let number = 10;
+
+// Pour un nombre aléatoire
+// On déclare deux variables dans lesquelles seront stokées une valeur minimale et une maximale
+let min = 0;
+let max = 50;
+
+//Math.floor nous permet de générer un chiffre sans virgule et Math.random randomise le processuss
+let random = Math.floor(Math.random() * (max - min)) + min;
+// console.log(random);
+
 // (console.log(number));
 
 // Un prompt apparaît pour informez l'utilisateur, ce dernier doit entrer un nombre
 userNumber = prompt("Découvres le bon numéro");
 
 // Nous ne conaissons pas le nombre de tentatives de l'utilisateur, on utilise une boucle WHILE
-while (userNumber != number) {
+while (userNumber != random) {
 // Dans cette boucle, nous déterminons les conditions (+ / -)    
-if (userNumber > number) {
+if (userNumber > random) {
 
     alert("TROP HAUT!");
     userNumber = prompt("Découvres le bon numéro");
 
-} if (userNumber < number) {
+} if (userNumber < random) {
 
     alert("TROP BAS!");
     userNumber = prompt("Découvres le bon numéro");
@@ -29,7 +39,7 @@ else {
 }
 
 // On sort de la boucle quand l'utilisateur trouve la bonne réponse
-if (userNumber == number) {
+if (userNumber == random) {
 
     alert("BRAVO!");
 
