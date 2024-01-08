74bc333558dda0a44f7332bc355fa97155d4f96d
kferrerux <ferrerkevin@hotmail.fr>
2024-01-08 15:25:34 +0100

    main.js fix

--- main.js
+++ main.js
@@ -5,36 +5,27 @@
 
 // Pour un nombre aléatoire
 // On déclare deux variables dans lesquelles seront stokées une valeur minimale et une maximale
-let min = 0;
+let min = 1;
 let max = 50;
 
 //Math.floor nous permet de générer un chiffre sans virgule et Math.random randomise le processuss
 let random = Math.floor(Math.random() * (max - min)) + min;
-// console.log(random);
-
-// (console.log(number));
 
 // Un prompt apparaît pour informez l'utilisateur, ce dernier doit entrer un nombre
-userNumber = prompt("Découvres le bon numéro");
+let userNumber = prompt("Découvres le bon numéro");
 
 // Nous ne conaissons pas le nombre de tentatives de l'utilisateur, on utilise une boucle WHILE
 while (userNumber != random) {
-// Dans cette boucle, nous déterminons les conditions (+ / -)    
-if (userNumber > random) {
+    // Dans cette boucle, nous déterminons les conditions (+ / -)    
+    if (userNumber > random) {
 
-    alert("TROP HAUT!");
-    userNumber = prompt("Découvres le bon numéro");
+        alert("TROP HAUT!");
+        
+    } else {
 
-} if (userNumber < random) {
-
-    alert("TROP BAS!");
-    userNumber = prompt("Découvres le bon numéro");
-}
-
-else {
-
-    userNumber = prompt("Découvres le bon numéro");
-}
+        alert("TROP BAS!");
+    }
+        userNumber = prompt("Découvres le bon numéro");
 
 }
 
